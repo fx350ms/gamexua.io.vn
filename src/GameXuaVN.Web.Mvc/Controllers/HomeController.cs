219d71ea337ff78d51c5960716423901e540a4d9
NGUYEN THANH TU <thanhtuk9acntt@hotmail.com>
2024-11-21 16:40:30 +0700

    add log file

--- src/GameXuaVN.Web.Mvc/Controllers/HomeController.cs
+++ src/GameXuaVN.Web.Mvc/Controllers/HomeController.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Abp.AspNetCore.Mvc.Authorization;
 using GameXuaVN.Controllers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace GameXuaVN.Web.Controllers
 {
    
     public class HomeController : GameXuaVNControllerBase
     {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _environment;
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+
         public ActionResult Index()
         {
+            ViewBag.Environment = _environment.EnvironmentName;
             return View();
         }
 
@@ -22,6 +35,14 @@ namespace GameXuaVN.Web.Controllers
         public ActionResult CommingSoon()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Ghi log thông tin về môi trường
+            _logger.LogInformation("HomeController is running in environment: {Environment}", _environment.EnvironmentName);
+            return Ok(new { Environment = _environment.EnvironmentName });
         }
     }
 }

9b7b03e258ffb51b8461159f5b6a22b4ebe526d3
NGUYEN THANH TU <thanhtuk9acntt@hotmail.com>
2024-11-23 08:09:20 +0700

    add

---
+++ src/GameXuaVN.Web.Mvc/wwwroot/view-resources/Views/Games/_CloneModal.js
@@ -0,0 +1,47 @@
+﻿(function ($) {
+    var _gameService = abp.services.app.game,
+        l = abp.localization.getSource('GameXuaVN'),
+        _$modal = $('#GameEditModal'),
+        _$form = _$modal.find('form');
+
+    function save() {
+        if (!_$form.valid()) {
+            return;
+        }
+
+        var game = _$form.serializeFormToObject();
+        game.roleNames = [];
+        var _$roleCheckboxes = _$form[0].querySelectorAll("input[name='role']:checked");
+        if (_$roleCheckboxes) {
+            for (var roleIndex = 0; roleIndex < _$roleCheckboxes.length; roleIndex++) {
+                var _$roleCheckbox = $(_$roleCheckboxes[roleIndex]);
+                game.roleNames.push(_$roleCheckbox.val());
+            }
+        }
+
+        abp.ui.setBusy(_$form);
+        _gameService.update(game).done(function () {
+            _$modal.modal('hide');
+            abp.notify.info(l('SavedSuccessfully'));
+            abp.event.trigger('game.edited', game);
+        }).always(function () {
+            abp.ui.clearBusy(_$form);
+        });
+    }
+
+    _$form.closest('div.modal-content').find(".save-button").click(function (e) {
+        e.preventDefault();
+        save();
+    });
+
+    _$form.find('input').on('keypress', function (e) {
+        if (e.which === 13) {
+            e.preventDefault();
+            save();
+        }
+    });
+
+    _$modal.on('shown.bs.modal', function () {
+        _$form.find('input[type=text]:first').focus();
+    });
+})(jQuery);

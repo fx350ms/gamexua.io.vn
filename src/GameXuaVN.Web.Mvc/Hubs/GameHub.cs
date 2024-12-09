7c448d698f0b7667af35b30e5ee27e3833e91ca0
NGUYEN THANH TU <thanhtuk9acntt@hotmail.com>
2024-12-09 13:38:08 +0700

    done join

--- src/GameXuaVN.Web.Mvc/Hubs/GameHub.cs
+++ src/GameXuaVN.Web.Mvc/Hubs/GameHub.cs
@@ -33,7 +33,7 @@ namespace GameXuaVN.Web.Hubs
 
 
 
-        private static ConcurrentDictionary<string, Room> Rooms = new ConcurrentDictionary<string, Room>();
+        ///  private static ConcurrentDictionary<string, Room> Rooms = new ConcurrentDictionary<string, Room>();
 
         public override Task OnConnectedAsync()
         {
@@ -52,56 +52,59 @@ namespace GameXuaVN.Web.Hubs
 
         private void Disconnect(string connectionId)
         {
-            var room = Rooms.Values.FirstOrDefault(r => r.Users.Any(u => u.ConnectionId == connectionId));
-            if (room == null) return;
+            //var room = Rooms.Values.FirstOrDefault(r => r.Users.Any(u => u.ConnectionId == connectionId));
+            //if (room == null) return;
 
-            var user = room.Users.FirstOrDefault(u => u.ConnectionId == connectionId);
-            if (user != null)
-            {
-                room.Users.Remove(user);
-            }
+            //var user = room.Users.FirstOrDefault(u => u.ConnectionId == connectionId);
+            //if (user != null)
+            //{
+            //    room.Users.Remove(user);
+            //}
 
-            // Gửi thông báo người dùng đã rời phòng
-            Clients.Group(room.Id).SendAsync("user-disconnected", user.UserId);
+            //// Gửi thông báo người dùng đã rời phòng
+            //Clients.Group(room.Id).SendAsync("user-disconnected", user.UserId);
 
-            if (!room.Users.Any())
-            {
-                // Nếu phòng trống, xóa phòng
-                Rooms.TryRemove(room.Id, out _);
-            }
-            else if (room.Owner.UserId == user.UserId)
-            {
-                // Nếu người rời là chủ phòng, chuyển quyền chủ phòng
-                room.Owner = room.Users.First();
-                Clients.Client(room.Owner.ConnectionId).SendAsync("set-isInitiator-true", room.SessionId);
-            }
+            //if (!room.Users.Any())
+            //{
+            //    // Nếu phòng trống, xóa phòng
+            //    Rooms.TryRemove(room.Id, out _);
+            //}
+            //else if (room.Owner.UserId == user.UserId)
+            //{
+            //    // Nếu người rời là chủ phòng, chuyển quyền chủ phòng
+            //    room.Owner = room.Users.First();
+            //    Clients.Client(room.Owner.ConnectionId).SendAsync("set-isInitiator-true", room.SessionId);
+            //}
         }
 
 
         public async Task CheckPresence(string roomId)
         {
-            if (Rooms.TryGetValue(roomId, out var room))
-            {
-                var userCount = room.Users.Count;
-                await Clients.Caller.SendAsync("check-presence-callback", true, roomId, userCount);
-            }
-            else
-            
-            {
-                await Clients.Caller.SendAsync("check-presence-callback", false, roomId, 0);
-            }
+            //if (Rooms.TryGetValue(roomId, out var room))
+            //{
+            //    var userCount = room.Users.Count;
+            //    await Clients.Caller.SendAsync("check-presence-callback", true, roomId, userCount);
+            //}
+            //else
+
+            //{
+            //    await Clients.Caller.SendAsync("check-presence-callback", false, roomId, 0);
+            //}
+
+            await Clients.Caller.SendAsync("check-presence-callback", false, roomId, 0);
         }
 
         public async Task OpenRoom(RoomData data)
         {
-            if (Rooms.TryGetValue(data.SessionId, out var existingRoom))
-            {
-                await Clients.Caller.SendAsync("open-room-callback", false, "ROOM_ALREADY_EXISTS");
-                return;
-            }
+            //if (Rooms.TryGetValue(data.SessionId, out var existingRoom))
+            //{
+            //    await Clients.Caller.SendAsync("open-room-callback", false, "ROOM_ALREADY_EXISTS");
+            //    return;
+            //}
 
             var room = new Room
             {
+                Id = data.RoomId.ToString(),
                 Domain = data.Extra.Domain,
                 GameId = data.Extra.GameId,
                 SessionId = data.SessionId,
@@ -111,14 +114,14 @@ namespace GameXuaVN.Web.Hubs
                 Password = data.Password.Trim(),
                 CoreVersion = data.CoreVer,
                 Users = new List<User>
-        {
-            new User
-            {
-                UserId = data.UserId,
-                ConnectionId = Context.ConnectionId,
-                ExtraData = data.Extra
-            }
-        },
+                {
+                    new User
+                    {
+                        UserId = data.UserId,
+                        ConnectionId = Context.ConnectionId,
+                        ExtraData = data.Extra
+                    }
+                },
                 Owner = new User
                 {
                     UserId = data.UserId,
@@ -126,8 +129,11 @@ namespace GameXuaVN.Web.Hubs
                     ExtraData = data.Extra
                 }
             };
+
             await Groups.AddToGroupAsync(Context.ConnectionId, room.Id);
             await Clients.Caller.SendAsync("open-room-callback", true, room.Id);
+            //await Groups.AddToGroupAsync(Context.ConnectionId, room.Id);
+            //await Clients.Caller.SendAsync("open-room-callback", true, room.Id);
             //if (Rooms.TryAdd(room.Id, room))
             //{
             //    await Groups.AddToGroupAsync(Context.ConnectionId, room.Id);
@@ -142,11 +148,11 @@ namespace GameXuaVN.Web.Hubs
 
         public async Task JoinRoom(RoomJoinData data)
         {
-            if (!Rooms.TryGetValue(data.SessionId, out var room))
-            {
-                await Clients.Caller.SendAsync("join-room-callback", false, "ROOM_NOT_FOUND");
-                return;
-            }
+            //if (!Rooms.TryGetValue(data.SessionId, out var room))
+            //{
+            //    await Clients.Caller.SendAsync("join-room-callback", false, "ROOM_NOT_FOUND");
+            //    return;
+            //}
 
             //if (room.CurrentParticipants >= room.MaxParticipantsAllowed)
             //{
@@ -161,44 +167,44 @@ namespace GameXuaVN.Web.Hubs
                 ExtraData = data.Extra
             };
 
-            room.Users.Add(newUser);
-            room.CurrentParticipants++;
+            //room.Users.Add(newUser);
+            //room.CurrentParticipants++;
 
-            await Groups.AddToGroupAsync(Context.ConnectionId, room.Id);
+            await Groups.AddToGroupAsync(Context.ConnectionId, data.RoomId);
 
-            await Clients.Group(room.Id).SendAsync("user-connected", data.UserId);
+            await Clients.Group(data.RoomId).SendAsync("user-connected", data.UserId);
             await Clients.Caller.SendAsync("join-room-callback", true, null);
         }
 
 
-        public async Task SetPassword(string roomId, string password)
-        {
-            if (!Rooms.TryGetValue(roomId, out var room)) return;
+        //public async Task SetPassword(string roomId, string password)
+        //{
+        //    if (!Rooms.TryGetValue(roomId, out var room)) return;
 
-            room.Password = password?.Trim();
-            await Clients.Caller.SendAsync("set-password-callback", true);
-        }
+        //    room.Password = password?.Trim();
+        //    await Clients.Caller.SendAsync("set-password-callback", true);
+        //}
 
-        public async Task ExtraDataUpdated(string roomId, object data)
-        {
-            if (!Rooms.TryGetValue(roomId, out var room)) return;
+        //public async Task ExtraDataUpdated(string roomId, object data)
+        //{
+        //    if (!Rooms.TryGetValue(roomId, out var room)) return;
 
-            room.ExtraData = data;
+        //    room.ExtraData = data;
 
-            // Gửi thông báo cập nhật dữ liệu tới tất cả các thành viên
-            await Clients.Group(room.Id).SendAsync("extra-data-updated", room.Owner.UserId, data);
-        }
+        //    // Gửi thông báo cập nhật dữ liệu tới tất cả các thành viên
+        //    await Clients.Group(room.Id).SendAsync("extra-data-updated", room.Owner.UserId, data);
+        //}
 
-        public async Task GetRemoteUserExtraData(string roomId, string userId)
-        {
-            if (!Rooms.TryGetValue(roomId, out var room)) return;
+        //public async Task GetRemoteUserExtraData(string roomId, string userId)
+        //{
+        //    if (!Rooms.TryGetValue(roomId, out var room)) return;
 
-            var user = room.Users.FirstOrDefault(u => u.UserId == userId);
-            if (user != null)
-            {
-                await Clients.Caller.SendAsync("extra-data-updated", user.ExtraData);
-            }
-        }
+        //    var user = room.Users.FirstOrDefault(u => u.UserId == userId);
+        //    if (user != null)
+        //    {
+        //        await Clients.Caller.SendAsync("extra-data-updated", user.ExtraData);
+        //    }
+        //}
 
         public Task CreateRoom(long roomId, int gameId, string roomName, string gameName)
         {
@@ -209,10 +215,7 @@ namespace GameXuaVN.Web.Hubs
 
         public async Task Netplay(string roomId, NetplayMessage msg)
         {
-            if (!Rooms.TryGetValue(roomId, out var room))
-            {
-                return; // Nếu không tìm thấy phòng, thoát
-            }
+          
 
             // Tạo bản sao tin nhắn và gắn thêm dữ liệu "extraData"
             var outMsg = new NetplayMessage
@@ -226,49 +229,49 @@ namespace GameXuaVN.Web.Hubs
             await Clients.Group(roomId).SendAsync("netplay", outMsg);
 
             // Kiểm tra nếu userLeft === true, thì xử lý ngắt kết nối
-            if (msg.Message?.UserLeft == true)
-            {
-                await DisconnectFromRoom(roomId, msg.Sender);
-            }
+            //if (msg.Message?.UserLeft == true)
+            //{
+            //    await DisconnectFromRoom(roomId, msg.Sender);
+            //}
         }
 
-        private async Task DisconnectFromRoom(string roomId, string userId)
-        {
-            // Kiểm tra nếu phòng tồn tại
-            if (!Rooms.TryGetValue(roomId, out var room))
-            {
-                return; // Không tìm thấy phòng
-            }
+        //private async Task DisconnectFromRoom(string roomId, string userId)
+        //{
+        //    // Kiểm tra nếu phòng tồn tại
+        //    if (!Rooms.TryGetValue(roomId, out var room))
+        //    {
+        //        return; // Không tìm thấy phòng
+        //    }
 
-            // Tìm người dùng trong danh sách phòng
-            var user = room.Users.FirstOrDefault(u => u.UserId == userId);
-            if (user == null)
-            {
-                return; // Không tìm thấy người dùng
-            }
+        //    // Tìm người dùng trong danh sách phòng
+        //    var user = room.Users.FirstOrDefault(u => u.UserId == userId);
+        //    if (user == null)
+        //    {
+        //        return; // Không tìm thấy người dùng
+        //    }
 
-            // Loại bỏ người dùng khỏi phòng
-            room.Users.Remove(user);
+        //    // Loại bỏ người dùng khỏi phòng
+        //    room.Users.Remove(user);
 
-            // Gửi thông báo người dùng đã rời phòng tới tất cả thành viên trong phòng
-            await Clients.Group(roomId).SendAsync("user-disconnected", userId);
+        //    // Gửi thông báo người dùng đã rời phòng tới tất cả thành viên trong phòng
+        //    await Clients.Group(roomId).SendAsync("user-disconnected", userId);
 
-            // Kiểm tra nếu phòng trống
-            if (!room.Users.Any())
-            {
-                // Nếu phòng trống, xóa phòng khỏi danh sách
-                Rooms.TryRemove(roomId, out _);
-            }
-            else if (room.Owner.UserId == userId)
-            {
-                // Nếu người rời là chủ phòng, chuyển quyền chủ phòng
-                room.Owner = room.Users.First();
-                await Clients.Client(room.Owner.ConnectionId).SendAsync("set-isInitiator-true", room.SessionId);
-            }
+        //    // Kiểm tra nếu phòng trống
+        //    if (!room.Users.Any())
+        //    {
+        //        // Nếu phòng trống, xóa phòng khỏi danh sách
+        //        Rooms.TryRemove(roomId, out _);
+        //    }
+        //    else if (room.Owner.UserId == userId)
+        //    {
+        //        // Nếu người rời là chủ phòng, chuyển quyền chủ phòng
+        //        room.Owner = room.Users.First();
+        //        await Clients.Client(room.Owner.ConnectionId).SendAsync("set-isInitiator-true", room.SessionId);
+        //    }
 
-            // Xóa người dùng khỏi nhóm SignalR
-            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
-        }
+        //    // Xóa người dùng khỏi nhóm SignalR
+        //    await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
+        //}
 
     }
 
@@ -287,7 +290,7 @@ namespace GameXuaVN.Web.Hubs
         public User Owner { get; set; }
         public object ExtraData { get; set; }
 
-        public string Id => $"{Domain}:{GameId}:{SessionId}";
+        public string Id { get; set; } //=> $"{Domain}:{GameId}:{SessionId}";
     }
 
     public class User
@@ -305,6 +308,8 @@ namespace GameXuaVN.Web.Hubs
         public string Password { get; set; }
         public string UserId { get; set; }
         public int CoreVer { get; set; }
+        public int RoomId { get; set; }
+
     }
 
     public class RoomExtra
@@ -316,6 +321,7 @@ namespace GameXuaVN.Web.Hubs
 
     public class RoomJoinData
     {
+        public string RoomId { get; set; }
         public RoomExtra Extra { get; set; }
         public string SessionId { get; set; }
         public string Password { get; set; }
